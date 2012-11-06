a68a1af9d1a561adc07a4d0e2fe8258eba04be90
Chris Bracco <chris@cbracco.me>
2012-11-06 14:11:15 -0500

    Added a few jQuery plugins to ensure equal height columns so floats don't fall beneath each other when the viewport is resized

--- js/main.js
+++ js/main.js
@@ -1,1 +1,18 @@
+jQuery(document).ready(function($) {
 
+	// Toggle baseline grid
+	$(".baseline").hide();
+
+	$(".btn-baseline button").on("click", function() {
+	    $(".baseline").toggle();
+	});
+
+	// Equal height columns
+	$(".grid-col").equalHeightColumns();
+});
+
+// Re-calculate column heights on resize
+$(window).smartresize(function() {
+	$(".grid-col").css("height", "auto");
+    $(".grid-col").equalHeightColumns();
+});
--- js/plugins.js
+++ js/plugins.js
@@ -1,4 +1,6 @@
-// Avoid `console` errors in browsers that lack a console.
+/**
+ * Avoid `console` errors in browsers that lack a console.
+ */
 (function() {
     var method;
     var noop = function noop() {};
@@ -21,4 +23,105 @@
     }
 }());
 
-// Place any jQuery/helper plugins in here.
+/**
+ * Equal height columns
+ * jQuery plugin
+ * @author Luka Soppermann
+ * https://github.com/lukasoppermann/fs-equal-height
+ */
+
+(function($) {
+    $.fn.equalHeightColumns = function(){
+        // create variables
+        var _this, _column = $(this), position_top, row_divs = new Array(), current_tallest, current_row_start, current_div;
+
+        // set column height to auto (so it can be calculate without padding)
+        _column.height('auto');
+
+        // loop through columns
+        _column.each(function(){
+            // cache selection
+            _this = $(this);
+
+            // get top position
+            position_top = _this.offset().top;
+
+            // check for new row
+            if(current_row_start != position_top)
+            {
+                var len = row_divs.length;
+
+                if( len > 1)
+                {
+                    // set heights for completed row
+                    for(var current_div = 0; current_div < len; current_div++)
+                    {
+                        row_divs[current_div].height(current_tallest);
+                    }
+                }
+
+                // set the variables for the new row
+                row_divs.length = 0; // empty the array
+                current_row_start = position_top;
+                current_tallest = _this.height();
+                row_divs.push(_this);
+            }
+
+            else
+            {
+                 // add column
+                 row_divs.push(_this);
+                 current_tallest = Math.max(current_tallest, _this.height());
+            }
+
+            // do the last row if more than 1 item in it
+            var len = row_divs.length;
+
+            if( len > 1)
+            {
+                for(var current_div = 0; current_div < len; current_div++)
+                {
+                     row_divs[current_div].height(current_tallest);
+                }
+            }
+        });
+
+        // return columns for chainability
+        return _column;
+    }
+})(jQuery);
+
+/**
+ * Smart window resize
+ * jQuery plugin
+ * @author Paul Irish
+ * http://paulirish.com/2009/throttled-smartresize-jquery-event-handler/
+ */
+
+(function($,sr) {
+
+  // debouncing function from John Hann
+  // http://unscriptable.com/index.php/2009/03/20/debouncing-javascript-methods/
+  var debounce = function (func, threshold, execAsap) {
+      var timeout;
+
+      return function debounced () {
+          var obj = this, args = arguments;
+          function delayed () {
+              if (!execAsap)
+                  func.apply(obj, args);
+              timeout = null;
+          };
+
+          if (timeout)
+              clearTimeout(timeout);
+          else if (execAsap)
+              func.apply(obj, args);
+
+          timeout = setTimeout(delayed, threshold || 100);
+      };
+  }
+    // smartresize
+    jQuery.fn[sr] = function(fn){  return fn ? this.bind('resize', debounce(fn)) : this.trigger(sr); };
+
+})(jQuery,'smartresize');
