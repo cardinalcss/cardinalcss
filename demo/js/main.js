be4e923d2a98ebfe2b7e2bc2a8d90aaa88a0c40e
Chris Bracco <chris@cbracco.me>
2012-11-15 14:16:01 -0500

    add more demo styles and copy

--- demo/js/main.js
+++ demo/js/main.js
@@ -1,7 +1,9 @@
+// When the DOM is ready,
 jQuery(document).ready(function($) {
 	// Toggle baseline grid
+	$(".bl-grid").hide();
 	$(".bl-grid-toggle .btn").on("click", function() {
 	    $("body").toggleClass("bl-grid-on");
-	    $(".bl-grid").toggle();
+	    $(".bl-grid").fadeToggle(200);
 	});
 });
--- demo/js/plugins.js
+++ demo/js/plugins.js
@@ -24,49 +24,134 @@
     }
 }());
 
-/*
- * Baseline.js 1.0
- * jQuery plugin
- * @author Daniel Eden
- * @source http://cbrac.co/ZvVGHE
+/**
+ * REM unit polyfill
+ * @author Chuck Carpenter
+ * @source http://cbrac.co/XCmW8V
  */
 
-(function($) {
-    $.fn.baseline = function(breakpoints) {
-        // Set up our variables, like a good little developer
-        var tall, newHeight, base, old = 0;
+(function (window, undefined) {
+    // test for REM unit support
+    var cssremunit =  function() {
+        var div = document.createElement( 'div' );
+        div.style.cssText = 'font-size: 1rem;';
 
-        return this.each(function(){
-            var $this = $(this); // Set the images as objects
+        return (/rem/).test(div.style.fontSize);
+    },
 
-            var setbase = function(breakpoints) { // The fun starts here
-                // Check if a single value or multiple breakpoints are given
-                        if (typeof breakpoints === 'number') {
-                            base = breakpoints;
-                        } else if (typeof breakpoints === 'object') {
-                            // Loop through the breakpoints and check which baseline to apply
-                            for (key in breakpoints) {
-                                var current = parseInt(key);
-                                if (document.width > current && current >= old) {
-                                    base = breakpoints[key];
-                                    old = current;
-                                }
-                            }
-                        }
+    // filter returned link nodes for stylesheets
+    isStyleSheet = function () {
+        var styles = document.getElementsByTagName('link'),
+            filteredStyles = [];
 
-                $this.css('maxHeight', 'none'); // Remove old max-height so that we can resize up as well as down
-                tall = $this.height(); // Grab the height
-                newHeight = Math.floor(tall / base) * base; // Make up a new height based on the baseline
-                $this.css('maxHeight', newHeight); // Set it!
+        for (i = 0; i < styles.length; i++) {
+             if ( styles[i].rel === 'stylesheet' )
+                filteredStyles.push( styles[i] );
+        }
+
+        return filteredStyles;
+    },
+
+    processSheets = function () {
+        var links = [];
+        sheets = isStyleSheet(); // search for link tags and confirm it's a stylesheet
+        sheets.og = sheets.length; // store the original length of sheets as a property
+        for( var i = 0; i < sheets.length; i++ ){
+            links[i] = sheets[i].href;
+            xhr( links[i], matchcss, i );
+        }
+    },
+
+    matchcss = function ( response, i ) { // collect all of the rules from the xhr response texts and match them to a pattern
+        var pattern = /[\w\d\.\,\:\[\]\^\>\/\<\+\~\|\-\_\$\#\"\'\/\*\\\=\s]+\{[\w\d\.\,\-\(\)\%\#\:\;\'\"\/\*\\\s]+\d*\.{0,1}\d+rem[\w\d\.\,\-\(\)\%\#\:\;\'\"\/\*\\\s]+\}/g, // find selectors that use rem in one or more of their rules
+            current = response.responseText.match(pattern),
+            remPattern =/\d*\.{0,1}\d+rem/g,
+            remCurrent = response.responseText.match(remPattern);
+        if( current !== null && current.length !== 0 ){
+            found = found.concat( current ); // save all of the blocks of rules with rem in a property
+            foundProps = foundProps.concat( remCurrent ); // save all of the properties with rem
+        }
+        if( i === sheets.og-1 ){
+            buildIt();
+        }
+    },
+
+    buildIt = function () { // first build each individual rule from elements in the found array and then add it to the string of rules.
+        var pattern = /[\w\d\.\,\-\(\)\%\#\:\'\"\/\*\\\s]+\d*\.{0,1}\d+rem[\w\d\.\,\-\(\)\%\#\:\'\"\/\*\\\s]*;/g; // find properties with rem values in them
+        for( var i = 0; i < found.length; i++ ){
+            rules = rules + found[i].substr(0,found[i].indexOf("{")+1); // save the selector portion of each rule with a rem value
+            var current = found[i].match( pattern );
+            for( var j = 0; j<current.length; j++ ){ // build a new set of with only the selector and properties that have rem in the value
+                rules = rules + current[j];
+                if( j === current.length-1 ){
+                    rules = rules + "\n}";
+                }
             }
+        }
 
-            setbase(breakpoints); // Call on load
+        parseIt();
+    },
 
-            $(window).resize(function(){ // And call again on resize.
-                setbase(breakpoints);
-            });
-        });
+    parseIt = function () { // replace each set of parentheses with evaluated content
+        for( var i = 0; i < foundProps.length; i++ ){
+            css[i] = Math.round( eval(foundProps[i].substr(0,foundProps[i].length-3)*fontSize) ) + 'px';
+        }
+        loadCSS();
+    },
+
+    loadCSS = function () { // replace and load the new rules
+        for( var i = 0; i < css.length; i++ ){ // only run this loop as many times as css has entries
+            if( css[i] ){
+                rules = rules.replace( foundProps[i],css[i] ); // replace old rules with our processed rules
+            }
+        }
+        rules = removeComments( rules );
+        var remcss = document.createElement( 'style' );
+        remcss.setAttribute( 'type', 'text/css' );
+        remcss.id = 'remReplace';
+        document.getElementsByTagName( 'head' )[0].appendChild( remcss );   // create the new element
+        remcss.styleSheet.cssText = rules; // IE8 will not support innerHTML on read-only elements, such as STYLE
+    },
+
+    xhr = function ( url, callback, i ) { // create new XMLHttpRequest object and run it
+        var xhr = getXMLHttpRequest();
+        xhr.open( 'GET', url, true );
+        xhr.send();
+        xhr.onreadystatechange = function() {
+            if ( xhr.readyState === 4 ){
+                callback(xhr, i);
+            } else { // callback function on AJAX error
+
+            }
+        };
+    },
+
+    removeComments =  function ( css ) {
+        return css.replace(/\/\*[\w\d\.\,\[\]\^\>\<\+\~\|\-\_\$\#\"\'\/\*\\\=\s\{\}\(\)]*\*\//g, "");
+    },
+
+    getXMLHttpRequest = function () { // we're gonna check if our browser will let us use AJAX
+        if(window.XMLHttpRequest){
+            return new XMLHttpRequest();
+        }
+    };
+
+    if( !cssremunit() ){ // this checks if the rem value is supported
+        var rules = '', // initialize the rules variable in this scope so it can be used later
+            sheets = [], // initialize the array holding the sheets for use later
+            found = [], // initialize the array holding the found rules for use later
+            foundProps = [], // initialize the array holding the found properties for use later
+            css = [], // initialize the array holding the parsed rules for use later
+            body = document.getElementsByTagName('body')[0],
+            fontSize = '';
+        if (body.currentStyle) {
+            fontSize = (body.currentStyle['fontSize'].replace('%', '') / 100) * 16; // IE8 returns the percentage while other browsers return the computed value
+        } else if (window.getComputedStyle)
+            fontSize = document.defaultView.getComputedStyle(body, null).getPropertyValue('font-size').replace('px', ''); // find font-size in body element
+        processSheets();
+    } else {
+        // do nothing, you are awesome and have REM support
     }
-}) (jQuery);
+})(window);
 
-// Place your extra jQuery/helper plugins here
+// Place your extra Javascript/jQuery plugins here
