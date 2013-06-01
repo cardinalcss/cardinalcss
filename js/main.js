17cb71f208c3ab6f04b4d7b7ed8681b5b2b43877
cbracco <chris@cbracco.me>
2013-06-01 10:02:15 -0400

    updated menu button

--- js/main.js
+++ js/main.js
@@ -1,6 +1,6 @@
 $(function() {
 	// Menu button toggle
-	$(".menu-button").on("click", function(){
+	$(".menu-button").on("click", function() {
 		$(".page").toggleClass("active");
 		$(".menu").toggleClass("active");
 	});
