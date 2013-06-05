7e25eca0744a20a18dd9028a56d9be7123cf7be8
cbracco <chris@cbracco.me>
2013-06-05 18:24:32 -0400

    replaced back to top unicode icon with an SVG icon

--- js/main.js
+++ js/main.js
@@ -4,11 +4,4 @@ $(function() {
 		$(".page").toggleClass("active");
 		$(".menu").toggleClass("active");
 	});
-
-	// Fallback to .png logo if SVG is not supported
-	if (!Modernizr.svg) {
-		$(".birdy").each(function() {
-			$(this).attr("src", $(this).data("src"));
-		});
-	}
 });
