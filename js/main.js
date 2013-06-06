ab2638049b2e8013d02405e9ac3c5db3f249514c
cbracco <chris@cbracco.me>
2013-06-06 15:03:44 -0400

    updated menu buttons to add scroll animation and .current classes

--- js/main.js
+++ js/main.js
@@ -4,4 +4,22 @@ $(function() {
 		$(".page").toggleClass("active");
 		$(".menu").toggleClass("active");
 	});
+
+	// Current state & scroll animation for menu items
+	var menu = $(".menu ul");
+	var menu_link = $(".menu a");
+
+	menu_link.on("click", function(e) {
+		// Prevent the default event
+		e.preventDefault();
+
+		// Animate to the corresponding section
+		$('html, body').animate({ scrollTop: $(this.hash).offset().top }, 500);
+
+		// Remove the “active” class from other parent items
+		menu.children().removeClass();
+
+		// Add the class to current item’s parent
+		$(this).parent().addClass("current");
+	});
 });
