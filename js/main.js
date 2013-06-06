5fcfa730f2faf13b9a2e5574ed776aac9a979eb9
cbracco <chris@cbracco.me>
2013-06-06 15:44:56 -0400

    made menu touch friendly; added animation to .btn_top

--- js/main.js
+++ js/main.js
@@ -1,25 +1,72 @@
 $(function() {
-	// Menu button toggle
-	$(".menu-button").on("click", function() {
-		$(".page").toggleClass("active");
-		$(".menu").toggleClass("active");
+	/**
+	 * Menu button toggle
+	 */
+	var menu_button = $(".menu-button");
+
+	$(".menu-button").on("touchstart click", function(e) {
+		// Prevent the default event
+		e.stopPropagation();
+		e.preventDefault();
+
+		var page = $(".page");
+
+		if(e.handled !== true) {
+			page.toggleClass("menu_open");
+
+			// Set e.handled to true
+			e.handled = true;
+		} else {
+			return false;
+		}
 	});
 
-	// Current state & scroll animation for menu items
+	/**
+	 * Current state & scroll animation for menu items
+	 */
 	var menu = $(".menu ul");
 	var menu_link = $(".menu a");
 
-	menu_link.on("click", function(e) {
+	menu_link.on("touchstart click", function(e) {
 		// Prevent the default event
+		e.stopPropagation();
 		e.preventDefault();
 
-		// Animate to the corresponding section
-		$('html, body').animate({ scrollTop: $(this.hash).offset().top }, 500);
+		if(e.handled !== true) {
+			// Animate to the corresponding section
+			$('html, body').animate({ scrollTop: $(this.hash).offset().top }, 500);
 
-		// Remove the “active” class from other parent items
-		menu.children().removeClass();
+			// Remove the “active” class from other parent items
+			menu.children().removeClass();
 
-		// Add the class to current item’s parent
-		$(this).parent().addClass("current");
+			// Add the class to current item’s parent
+			$(this).parent().addClass("current");
+
+			// Set e.handled to true
+			e.handled = true;
+		} else {
+			return false;
+		}
+	});
+
+	/**
+	 * Back to top button
+	 */
+	var btn_top = $(".btn_top");
+
+	btn_top.on("touchstart click", function(e){
+		// Prevent the default event
+		e.stopPropagation();
+		e.preventDefault();
+
+		if(e.handled !== true) {
+			// Animate to the corresponding section
+			$('html, body').animate({ scrollTop: $(this.hash).offset().top }, 500);
+
+			// Set e.handled to true
+			e.handled = true;
+		} else {
+			return false;
+		}
 	});
 });
