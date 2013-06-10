babc46c40c5f1cf082fd662e40cc783387681db6
cbracco <chris@cbracco.me>
2013-06-10 17:43:10 -0400

    prettied up the js a bit; some layout changes;

--- js/main.js
+++ js/main.js
@@ -1,84 +1,68 @@
-$(function() {
-	/**
-	 * Menu button toggle
-	 */
-	var menu_button = $(".menu-button");
-
-	$(".menu-button").on("touchstart click", function(e) {
-		// Prevent the default event
-		e.stopPropagation();
-		e.preventDefault();
-
-		var page = $(".page");
-
-		if(e.handled !== true) {
-			page.toggleClass("menu_open");
-
-			// Set e.handled to true
-			e.handled = true;
-		} else {
-			return false;
-		}
-	});
-
-	/**
-	 * Current state & scroll animation for menu items
-	 */
-	var menu = $(".menu ul");
-	var menu_link = $(".menu a");
-
-	// Set a flag for drag
-	var flag = false;
-	menu_link.on("touchmove", function() {
-		flag = true;
-	});
-
-	menu_link.on("touchend click", function(e) {
-		// Prevent the default event
-		e.stopPropagation();
-		e.preventDefault();
-
-		// If it was a drag, do nothing, and reset the flag
-		if(flag) {
+$(function()
+	{
+		/**
+		 * Sidebar menu
+		 */
+		var html_body = $("html, body"),
+			page = $(".page"),
+			menu = $(".menu"),
+			menu_list = $(".menu ul"),
+			menu_button = $(".menu-button"),
+			menu_link = $(".menu a"),
 			flag = false;
-			return;
-		} else {
-			if(e.handled !== true) {
-				// Animate to the corresponding section
-				$('html, body').scrollTop($(this.hash).offset().top);
 
-				// Remove the “active” class from other parent items
-				menu.children().removeClass();
+		// Set a flag on drag
+		menu_link.on("touchmove", function() { flag = true; });
 
-				// Add the class to current item’s parent
-				$(this).parent().addClass("current");
+		// Move to appropriate content section on tap / click
+		menu_link.on("touchend click", function(e)
+			{
+				// Prevent the default event
+				e.stopPropagation();
+				e.preventDefault();
 
-				// Set e.handled to true
-				e.handled = true;
-			} else {
-				return false;
+				// If it was a drag, do nothing, and reset the flag
+				if(flag) {
+					flag = false;
+					return;
+				} else {
+					if(e.handled !== true) {
+						// Navigate to the corresponding section
+						html_body.scrollTop($(this.hash).offset().top);
+
+						// Remove the “active” class from other parent items
+						menu_list.children().removeClass();
+
+						// Add the class to current item’s parent
+						$(this).parent().addClass("current");
+
+						// Set e.handled to true
+						e.handled = true;
+					} else {
+						return false;
+					}
+				}
 			}
-		}
-	});
+		);
 
-	/**
-	 * Back to top button
-	 */
-	var btn_top = $(".btn_top");
+		/**
+		 * Sidebar menu button toggle
+		 */
+		menu_button.on("touchstart click", function(e)
+			{
+				// Prevent the default event
+				e.stopPropagation();
+				e.preventDefault();
 
-	btn_top.on("touchstart click", function(e){
-		// Prevent the default event
-		e.stopPropagation();
-		e.preventDefault();
+				if(e.handled !== true) {
+					page.toggleClass("menu_open");
 
-		if(e.handled !== true) {
-			// Animate to the corresponding section
-			$('html, body').scrollTop($(this.hash).offset().top);
-
-			// Set e.handled to true
-			e.handled = true;
-		} else {
-			return false;
-		}
-	});
-});
+					// Set e.handled to true
+					e.handled = true;
+				} else {
+					return false;
+				}
+			}
+		);
+	}
+);
