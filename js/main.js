8a3e8c5977fbe3d3ed598d78304e439fd2227fbe
cbracco <chris@cbracco.me>
2013-06-11 15:43:31 -0400

    updated js to prevent body scroll when scrolling inside the menu

--- js/main.js
+++ js/main.js
@@ -4,7 +4,7 @@ $(function()
 		 * Sidebar menu
 		 */
 
-		// Set variables
+		// Global variables
 		var html_body = $("html, body"),
 			page = $(".page"),
 			menu = $(".menu"),
@@ -12,6 +12,39 @@ $(function()
 			menu_button = $(".menu-button"),
 			menu_link = $(".menu a"),
 			flag = false;
+
+		// Prevent scrolling the entire page while scrolling inside the menu
+		// http://cbrac.co/1bt2GJg
+		menu.on('DOMMouseScroll mousewheel touchmove', function(e)
+			{
+				// Scroll variables
+				var $this = $(this),
+					scroll_top = this.scrollTop,
+					scroll_height = this.scrollHeight,
+					height = $this.height(),
+					delta = (e.type == 'DOMMouseScroll' ?
+						e.originalEvent.detail * -40 : /* Firefox */
+						e.originalEvent.wheelDelta),   /* The rest */
+					up = delta > 0;
+
+				// Prevent the scroll event
+				var prevent = function() {
+					e.stopPropagation();
+					e.preventDefault();
+					e.returnValue = false;
+					return false;
+				}
+
+				// Cancel the event if the scrollable area is at top or bottom
+				if (!up && -delta > scroll_height - height - scroll_top) {
+					$this.scrollTop(scroll_height);
+					return prevent();
+				} else if (up && delta > scroll_top) {
+					$this.scrollTop(0);
+					return prevent();
+				}
+			}
+		);
 
 		// Set a flag on drag
 		menu_link.on("touchmove", function() { flag = true; });
