08e3b41505a722c8eead732bcbd569b06507344f
cbracco <chris@cbracco.me>
2014-01-16 23:11:27 -0500

    Revert "Merge branch 'docs-2.0.0' into gh-pages"
    This reverts commit 4fddcd3d076aa9c04105792f0c23446772a2eb79, reversing
    changes made to 722b91e10ad1d8962758a730060e14648a573ba8.

--- js/plugins.js
+++ js/plugins.js
@@ -3,7 +3,6 @@
  */
 
 (function() {
-<<<<<<< HEAD
 	var method;
 	var noop = function noop() {};
 	var methods = [
@@ -25,37 +24,12 @@
 	}
 }());
 
-=======
-    var method;
-    var noop = function noop() {};
-    var methods = [
-        'assert', 'clear', 'count', 'debug', 'dir', 'dirxml', 'error',
-        'exception', 'group', 'groupCollapsed', 'groupEnd', 'info', 'log',
-        'markTimeline', 'profile', 'profileEnd', 'table', 'time', 'timeEnd',
-        'timeStamp', 'trace', 'warn'
-    ];
-    var length = methods.length;
-    var console = (window.console = window.console || {});
-
-    while (length--) {
-        method = methods[length];
-
-        // Only stub undefined methods.
-        if (!console[method]) {
-            console[method] = noop;
-        }
-    }
-}());
-
-
->>>>>>> docs-2.0.0
 /**
  * Grab the latest version tag from GitHub and append it
  */
 
 function updateVersion() {
 
-<<<<<<< HEAD
 	$.ajax({
 		url: "https://api.github.com/repos/cbracco/cardinal/tags",
 		dataType: "jsonp",
@@ -67,18 +41,5 @@ function updateVersion() {
 			});
 		}
 	});
-=======
-    $.ajax({
-        url: "https://api.github.com/repos/cbracco/cardinal/tags",
-        dataType: "jsonp",
-        timeout: 60 * 1000,
-        success: function(results) {
-            var tag = results.data;
-            $(".version").each(function() {
-                $(this).append("&nbsp;<span class=\"version\">" + tag[0]["name"] + "</span>");
-            });
-        }
-    });
->>>>>>> docs-2.0.0
 
 }
